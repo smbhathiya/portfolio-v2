720ca88d4a2f27d4b3097f16f2b47234bc8318fc
smbhathiya <smbhathiya0@gmail.com>
2024-11-03 16:07:10 +0530

    design issues fixed

--- app/components/HeroSection.jsx
+++ app/components/HeroSection.jsx
@@ -13,16 +13,16 @@ const HeroSection = () => {
                     initial={{ opacity: 0, scale: 0.5 }}
                     animate={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.5 }}
-                    className="col-span-8 place-self-center text-center sm:text-left"
+                    className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
                 >
                     <h1 className="text-white mb-4 text-3xl sm:text-4xl lg:text-7xl lg:leading-normal font-extrabold">
                         <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary to-secondary">
                             Hello, I&apos;m{" "}
                         </span>
-                        <br />
+                        <br></br>
                         <TypeAnimation
                             sequence={[
-                                "Bhathiya Lakshan",
+                                "Bhathiya",
                                 2000,
                                 "Backend Developer",
                                 2000,
@@ -34,21 +34,19 @@ const HeroSection = () => {
                             repeat={Infinity}
                         />
                     </h1>
-                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-2xl lg:mb-8 font-bold">
+                    <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl lg:mb-8 font-bold">
                         Full-Stack Engineer specializing in Next.js & React for the frontend and .NET for the backend
                     </p>
                     <div>
                         <Link
                             href="/#contact"
-                            aria-label="Hire Me"
-                            className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-btn_primary hover:bg-slate-600 duration-200 text-white font-bold"
+                            className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-btn_primary hover:bg-slate-600 duration-200 text-white"
                         >
                             Hire Me
                         </Link>
                         <Link
                             href="/"
-                            aria-label="Download CV"
-                            className="px-1 py-1 w-full sm:w-fit rounded-full text-background font-bold mt-3"
+                            className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-primary text-background hover:bg-secondary font-bold mt-3"
                         >
                             <span className="inline-block bg-primary hover:bg-secondary rounded-full px-5 py-2">
                                 Download CV
@@ -62,14 +60,13 @@ const HeroSection = () => {
                     transition={{ duration: 0.5 }}
                     className="col-span-4 place-self-center mt-4 lg:mt-0"
                 >
-                    <div className="rounded-full w-[250px] h-[250px] lg:w-[400px] bg-primary lg:h-[400px] relative mx-auto sm:mt-0">
+                    <div className="rounded-full  w-[250px] h-[250px] lg:w-[400px] bg-primary lg:h-[400px] relative mx-auto sm:mt-0">
                         <Image
                             src="/images/hero-image-2.png"
-                            alt="Bhathiya Lakshan"
+                            alt="hero image"
                             className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                             width={450}
                             height={450}
-                            priority
                         />
                     </div>
                 </motion.div>
