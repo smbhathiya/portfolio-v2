5739a497d3d41d502b1b5cf8b9ed228a6988b781
Bhathiya Lakshan <148926405+smbhathiya@users.noreply.github.com>
2024-11-06 13:08:41 +0530

    contact section added

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import HeroSection from "@/app/components/HeroSection";
 import Navbar from "@/app/components/Navbar";
 import Footer from "@/app/components/Footer";
 import AboutSection from "@/app/components/AboutSection";
+import ContactSection from "@/app/components/ContactSection";
 
 export default function Home() {
     return (
@@ -10,6 +11,7 @@ export default function Home() {
             <div className="container mt-24 mx-auto px-12 py-4">
                 <HeroSection />
                 <AboutSection/>
+                <ContactSection/>
                 <Footer/>
                 </div>
         </main>
