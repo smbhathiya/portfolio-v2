23a2149cd4c30e357719eb270d99a11bcf879d0a
Bhathiya Lakshan <148926405+smbhathiya@users.noreply.github.com>
2024-12-02 10:07:51 +0530

    skill section created

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,8 +6,8 @@ import { motion, AnimatePresence } from "framer-motion";
 const navItems = [
     { href: "/", label: "Home" },
     { href: "/#about", label: "About" },
+    { href: "/#skills", label: "Skills" },
     { href: "/#projects", label: "Projects" },
-    { href: "/#services", label: "Services" },
     { href: "/#contact", label: "Contact" },
 ];
 
