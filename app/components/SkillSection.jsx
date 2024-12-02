860bae3d61d59500215bddf5b5189258578f6e38
Bhathiya Lakshan <148926405+smbhathiya@users.noreply.github.com>
2024-12-02 08:59:22 +0530

    fixed design issues

---
+++ app/components/SkillSection.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class SkillSection extends Component {
+    render() {
+        return (
+            <div>
+                HELLO
+            </div>
+        );
+    }
+}
+
+export default SkillSection;
